4ff2c2297081139ca4e83f4fc20645d399941056
Maria Defarges <mariadefarges@MacBook-Pro-de-Maria-5.local>
2024-05-11 12:29:53 +0200

    visual modifications

--- app.R
+++ app.R
@@ -15,107 +15,126 @@ library(DT)
 library(RHRV)
 library(RHRVEasy)
 library(shinyFiles)
-#library(shinythemes)
+library(shinythemes)
 library(shinyjs)
 library(shinyalert)
 library(DT)
+library(future)
 
 #Sys.setlocale("LC_NUMERIC", "en_US.UTF-8")
 
-
-ui <- fluidPage(
-  useShinyjs(),
-
-  navbarPage(
-    title = "RHRV Easy Analysis",
-    tabsetPanel(id = "tabs",
-                tabPanel("Settings", fluid = TRUE,
-                         sidebarLayout(
-                           sidebarPanel(
-                             actionButton(inputId = "startButton", label = "Start Analysis", class="btn-lg"),
-                             hr(),
-                             h4("Select folders"),
-                             shinyDirButton('dirchoose','Choose...','Choose a folder from your home unit'),
-                             actionButton("addfolderButton", "Add folder", class = "btn btn-primary"), #class = estilo y color
-                             hr(),
-                             actionButton(inputId = "restartButton", label = "Restart folder selection"),
-                             h5("Selected folders: "),
-                             uiOutput("selectedFolders"),
-                           ),
-                           mainPanel(
-                             tabsetPanel(
-                               tabPanel("Beat filtering", fluid = TRUE,
-                                        br(),
-                                        sliderInput("bmpId", "Allowable Heart Rate range", min = 20, max = 220, value = c(25,180)),
-                                        numericInput(inputId = "longId", label = "Normal Resting Heart Rate", min = 30, max = 130, value = 50),
-                                        numericInput(inputId = "lastId", label = "Maximum % of Heart Rate Change ", min = 1, max = 24, value = 10)
-                               ),
-                               tabPanel("Time domain", fluid = TRUE,
-                                        br(),
-                                        numericInput(inputId = "sizeId", label = "Window size", min = 100, max = 700, value = 300),
-                                        numericInput(inputId = "intervalId", label = "Histogram width for triangular interpolation", min = 5, max = 10, value = 7.8125),
-                               ),
-
-                               tabPanel("Power Bands", fluid = TRUE,
-                                        br(),
-                                        fluidRow(
-                                          column(6,
-                                                 #indexfreqanalysis
-                                                 selectInput(inputId = "typeId", label = "Calculation type", choices = c("Wavelet", "Fourier"), selected = "Fourier"),
-                                                 numericInput(inputId = "bandtoleranceId", label = "Band Tolerance", min = 0, max = 100, value = 0.01, step = 0.01),
-                                                 numericInput(inputId = "freqhrId", label = "Interpolation frequency", min = 0.05, max = 40, value = 4),
-                                          ),
-                                          column(6,
-                                                 sliderInput(inputId = "ULFId", label = "Ultra Low Frequency Band range", min = 0, max = 1, value = c(0,0.03)),
-                                                 sliderInput(inputId = "VLFId", label = "Very Low Frequency Band range", min = 0, max = 1, value = c(0.03,0.05)),
-                                                 sliderInput(inputId = "LFId", label = "Low Frequency Band range", min = 0, max = 1, value = c(0.05, 0.15)),
-                                                 sliderInput(inputId = "HFId", label = "High Frequency Band range", min = 0, max = 1, value = c(0.15,0.4))
-                                          )
-                                        )
-                               )
-                             )
-                           )
-                         ),
+ui <- fluidPage(theme = shinytheme("flatly"),
+                useShinyjs(),
+                tags$head(
+                  tags$link(rel = "stylesheet", href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"),
+                  tags$style(HTML("
+                  .btn.btn-default {
+                  padding: 6px 9px;
+                  }
+                "))
                 ),
-                tabPanel(
-                  "Dataframes",
-                  fluid = TRUE,
-                  hidden(
-                    div(id = "hiddendataframes",
-                        h4("Save to an Excel Spreadsheet"),
-                        shinyDirButton('folderchoose','Select...','Select the folder where the file will be saved'),
-                        actionButton(inputId = "saveexcelButton", label = "Save", class = "btn btn-primary"),
-                        hr(),
-                        tabsetPanel(
-                          tabPanel("HRV indices", fluid = TRUE,
-                                   nav_panel(title = "HRV indices",
-                                      DTOutput(outputId = "data_table1", width = "100%")
-                                      ),
-                                   ),
-                          tabPanel("Statistics", fluid = TRUE,
-                                     div(id = "hiddenstatistics",
-                                         nav_panel(title = "Statistics",
-                                                   div(id = "additionalmessage",
-                                                       h5("Click on each of the rows to see the Post Hoc tests"),
-                                                   ),
-                                                   tags$head(
-                                                     tags$style(HTML("
+                navbarPage(
+                  title = "HRV Analysis",
+                  tabsetPanel(id = "tabs",
+                              tabPanel("Settings", fluid = TRUE,
+                                       sidebarLayout(
+                                         sidebarPanel(
+                                           column(width = 12, align = "center",
+                                                  actionButton(inputId = "startButton", label = "START ANALYSIS", class="btn-info"),
+                                                  tags$hr(style = "border: none; background-color: transparent; margin-top: 10px; margin-bottom: 10px;"),
+                                           ),
+                                           h6("Select the following boxes if you want to perform a non linear analysis."),
+                                           h6("WARNING: Please note that this process will take several hours to complete"),
+                                           checkboxInput("nonlinear", tags$span("Non Linear analysis", style = "font-size: 14px;"), value = FALSE),
+                                           checkboxInput("RQA", tags$span("RQA", style = "font-size: 15px;"), value = FALSE),
+                                           h5("Select folders:"),
+                                           shinyDirButton('dirchoose','Choose...','Choose a folder from your home unit'),
+                                           actionButton("addfolderButton", "Add folder", class = "btn btn-primary"), #class = estilo y color
+                                           actionButton(inputId = "restartButton", label = "", icon = icon("redo"), style = "font-size: 13px;"),
+                                           tags$hr(style = "border: none; background-color: transparent; margin-top: 10px; margin-bottom: 10px;"),
+                                           uiOutput("selectedFolders", style = "font-size: 15px;"),
+                                         ),
+
+
+
+
+                                         mainPanel(
+                                           fluidRow(
+                                             tabsetPanel(
+                                               tabPanel("Beat filtering", fluid = TRUE,
+                                                        br(),
+                                                        sliderInput("bmpId", "Allowable Heart Rate range", min = 20, max = 220, value = c(25,180)),
+                                                        numericInput(inputId = "longId", label = "Normal Resting Heart Rate", min = 30, max = 130, value = 50),
+                                                        numericInput(inputId = "lastId", label = "Maximum % of Heart Rate change ", min = 1, max = 24, value = 10)
+                                               ),
+                                               tabPanel("Time domain", fluid = TRUE,
+                                                        br(),
+                                                        numericInput(inputId = "sizeId", label = "Window size", min = 100, max = 700, value = 300),
+                                                        numericInput(inputId = "intervalId", label = "Histogram width for triangular interpolation", min = 5, max = 10, value = 7.8125),
+                                               ),
+
+                                               tabPanel("Power Bands", fluid = TRUE,
+                                                        br(),
+                                                        fluidRow(
+                                                          column(6,
+                                                                 #indexfreqanalysis
+                                                                 selectInput(inputId = "typeId", label = "Calculation type", choices = c("Wavelet", "Fourier"), selected = "Fourier"),
+                                                                 numericInput(inputId = "bandtoleranceId", label = "Band Tolerance", min = 0, max = 100, value = 0.01, step = 0.01),
+                                                                 numericInput(inputId = "freqhrId", label = "Interpolation frequency", min = 0.05, max = 40, value = 4),
+                                                          ),
+                                                          column(6,
+                                                                 sliderInput(inputId = "ULFId", label = "Ultra Low Frequency Band range", min = 0, max = 1, value = c(0,0.03)),
+                                                                 sliderInput(inputId = "VLFId", label = "Very Low Frequency Band range", min = 0, max = 1, value = c(0.03,0.05)),
+                                                                 sliderInput(inputId = "LFId", label = "Low Frequency Band range", min = 0, max = 1, value = c(0.05, 0.15)),
+                                                                 sliderInput(inputId = "HFId", label = "High Frequency Band range", min = 0, max = 1, value = c(0.15,0.4))
+                                                          )
+                                                        )
+                                               )
+                                             ),
+                                           )
+
+                                         )
+                                       ),
+                              ),
+                              tabPanel(
+                                "Dataframes",
+                                fluid = TRUE,
+                                hidden(
+                                  div(id = "hiddendataframes",
+                                      h4("Save to an Excel Spreadsheet"),
+                                      shinyDirButton('folderchoose','Select...','Select the folder where the file will be saved'),
+                                      actionButton(inputId = "saveexcelButton", label = "Save", class = "btn btn-primary"),
+                                      hr(),
+                                      tabsetPanel(
+                                        tabPanel("HRV indices", fluid = TRUE,
+                                                 nav_panel(title = "HRV indices",
+                                                           DTOutput(outputId = "data_table1", width = "100%")
+                                                 ),
+                                        ),
+                                        tabPanel("Statistics", fluid = TRUE,
+                                                 div(id = "hiddenstatistics",
+                                                     nav_panel(title = "Statistics",
+                                                               div(id = "additionalmessage",
+                                                                   h5("Click on each of the rows to see the Post Hoc tests"),
+                                                               ),
+                                                               tags$head(
+                                                                 tags$style(HTML("
                                                       .dataTable tbody tr:hover {
                                                       cursor: pointer;
                                                       }
                                                       "))
-                                                   ),
-                                                       DTOutput(outputId = "data_table2", width = "100%")
+                                                               ),
+                                                               DTOutput(outputId = "data_table2", width = "100%")
 
-                                         )
-                                     ),
-                          ),
-                        )
-                    )
+                                                     )
+                                                 ),
+                                        ),
+                                      )
+                                  )
+                                )
+                              )
                   )
-                )
-                )
-  ),
+                ),
 )
 
 js <- function(id){
@@ -130,11 +149,39 @@ js <- function(id){
 
 
 server <- function(input, output, session) {
+  #Choose folders functions
   volumes <- c(Home = fs::path_home(), "R Installation" = R.home(), getVolumes()())
   shinyDirChoose(input, 'dirchoose', session=session, root=volumes, filetypes=c('txt'))
   shinyDirChoose(input, 'folderchoose', session=session, root=volumes, filetypes=c('txt'))
   dirlist <- reactiveVal(list())
 
+  observeEvent(input$addfolderButton, {
+    directories <- file.path((parseDirPath(volumes, input$dirchoose)))
+    updateddir <- c(dirlist(), directories)
+    dirlist(updateddir)
+  })
+
+  #Visualize the name of the folders selected
+  output$selectedFolders <- renderUI({
+    if (input$addfolderButton>0) {
+      foldersnames <- sapply(dirlist(), basename)
+      lapply(foldersnames, function(x) {
+        div(
+          class = "icon",
+          icon("folder"),
+          x
+        )
+      })
+    }
+    else {
+      return("Server is ready for calculation.")
+    }
+  })
+  observeEvent(input$restartButton, {
+    dirlist(list())
+  })
+
+  #Negative values of the numeric inputs in red
   observeEvent(input$typeId, {
     if (input$typeId == "Fourier") {
       shinyjs::disable("bandtoleranceId")
@@ -144,7 +191,6 @@ server <- function(input, output, session) {
       updateNumericInput(session, "bandtoleranceId", value = 0.01)
     }
     })
-
 
     observeEvent(input$longId, {
     if (input$longId == "" || !is.numeric(input$longId)) {
@@ -215,34 +261,6 @@ server <- function(input, output, session) {
 
 
 
-
-
-
-  observeEvent(input$addfolderButton, {
-    directories <- file.path((parseDirPath(volumes, input$dirchoose)))
-    updateddir <- c(dirlist(), directories)
-    dirlist(updateddir)
-  })
-
-  #Visualize the name of the folders selected
-  output$selectedFolders <- renderUI({
-    if (input$addfolderButton>0) {
-      foldersnames <- sapply(dirlist(), basename)
-      lapply(foldersnames, function(x) {
-        div(
-          class = "icon",
-          icon("folder"),
-          x
-        )
-      })
-    }
-    else {
-      return("Server is ready for calculation.")
-    }
-  })
-  observeEvent(input$restartButton, {
-    dirlist(list())
-  })
 
   observeEvent(input$startButton, {
     runjs('$("#hiddendataframes").hide();')
